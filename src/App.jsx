e765ea0ddf0a3027a8159cbeea3fe3a141054384
Alejandro Perez <aperezbanks@gmail.com>
2025-05-04 22:54:47 +0200

    probando cosillas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-
+import Header from './componentes/Header';
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+    <>
+    <Header />
+    <div className="bg-primary min-h-screen flex items-center justify-center bg-blue-50">
       <div className="text-center">
-        <h1 className="text-5xl font-bold text-pink-600 mb-4">
+        <h1 className="text-5xl font-bold text-white mb-4">
           Inventive
         </h1>
         <p className="text-black-500 font-bold p-4 rounded-lg">
@@ -15,6 +17,7 @@ function App() {
         </p>
       </div>
     </div>
+    </>
   );
 }
 
