83c04160b98a411a71146069a3cf733588ad33ef
Alejandro Perez <aperezbanks@gmail.com>
2025-05-04 22:09:00 +0200

    Version con React  + Vite + Tailwind, limpiado y funcionando

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-pink-600 mb-4">
+          Inventive
+        </h1>
+        <p className="text-black-500 font-bold p-4 rounded-lg">
+          La app que te hace las facturas más fáciles con solo una foto.
+          Ahora con Tailwind dejandolo bonito 
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
